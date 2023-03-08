205f122bb250ecaf365f8144f0eee857387b8649
jordangm94 <jordan.guerrero.martinez@gmail.com>
2023-03-07 21:18:33 -0500

    Worked through arrays in typescript in app.ts file.

--- app.ts
+++ app.ts
@@ -14,7 +14,15 @@
 
 const person = {
     name: 'Jordan',
-    age: 28
+    age: 28, 
+    hobbies: ['gaming', 'sports'] //Adding hobbies to learn about arrays in typescript
 }
 
+let favouriteActivities: string[]; //This means everything in aray must be a string, if you want to have a mixed array, could infer any[]
+favouriteActivities = ['sports'];
+
 console.log(person)
+
+for (let hobby of person.hobbies) { //In for loop, notice if you hover over hobby or person.hobbies, in the loop it knows what the type of each variable will be!
+    console.log(hobby.toUpperCase())//Since it knows hobby will be a string, can apply string methods/prototypes and are suggested by intellisense!
+}
