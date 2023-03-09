5c02469c2136cb4e6eaaccb55dd05d63f99b5011
jordangm94 <jordan.guerrero.martinez@gmail.com>
2023-03-08 21:27:07 -0500

    Introduced function type and void into app.ts.

--- app.ts
+++ app.ts
@@ -1,58 +1,33 @@
-// //Diving into the type of UNIONS
+//Diving into functions, let's recreate the add function
 
-// //Here is our add function from the basics. Let's say we want to be more flexible with what we accept here
-
-// //Let's say we want to adjust the function so it doesn't only add numbers, but it can also concatenate strings as well.
-
-// function combine(input1: number, input2: number) {
-//     //Here we are doing the math caculation before hand. That way we can pass it into the console log, instead of doing n1 + n2 in console log which would coancatenate numbers together due to presence of string in resultphrase
-//     const result = input1 + input2
-//     return result;
-// }
-
-// const combinedAges = combine(30, 26) // This will work and add the numbers
-// console.log(combinedAges)
-
-// //However, what if we want to add a string to concatenate
-
-// const combinedNames = combine('Jordan', 'Nikita') // See error because combine only accepts number, could change function take strings, but then first function call will fail
-
-///////////////////////////////////
-
-//Therefore to deal with these issues we use union types. This allows us to state that the input will take in more than one type
-
-type Combinable = number | string; //This is a type alias, store the union type that the parameter must follow in a type variable, that way can just plug this in and don't need to repeat code. Helps us write code quicker and ensure we don't run into typo issues.
-type ConversionDescriptors = 'as-number' | 'as-text'
-
-function combine(input1: Combinable, input2: Combinable, resultConversion: ConversionDescriptors) { //Resultconversion is example of literal types used in conjunction with union types, they allow for an EXACT value whether string or number to be required as argument for parameter
-    //If we just use union, input1 + input2 will be underlined because typescript doesn't read what is in the union, just assumes it can be anything in which case you couldn't add a boolean and a string or a boolean and a number, so we need to work around
-
-    let result;
-
-    //Here we check if input is all number types, add them
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
-    } else {
-        //If input is not all number types, can't risk concatinating number and string so make everything a string for consistency and accurate results
-        result = input1.toString() + input2.toString()
-    
-    }
-
-    //Apply a final conversion based on third paramter resultConversion that is passed to function
-    if (resultConversion === 'as-number') {
-        return +result
-    } else {
-        return result.toString()
-    }
-
-    return result;
+function add(n1: number, n2: number) {
+    return n1 + n2; //Return actually has a type and it is what you designated, if you hover over function it will tell you "function add(n1: number, n2: number): number", the number at the end is the return type
 }
 
-const combinedAges = combine(30, 26, 'as-number') // This will work and add the numbers
-console.log(combinedAges)
+//The return type can be inferred based on what the function is returning, or it can be explicitly stated like this: 
 
-const combinedStringAges = combine('30', '26', 'as-number')
-console.log(combinedStringAges)
+// function add(n1: number, n2: number): number {
+//     return n1 + n2; //Return actually has a type and it is what you designated, if you hover over function it will tell you "function add(n1: number, n2: number): number", the number at the end is the return type
+// }
 
-const combinedNames = combine('Jordan', 'Nikita', 'as-text')
-console.log(combinedNames)
+//However please note, it is better to let typescript do its job with type inference
+
+///////////////////Introduction to new type specific to RETURN
+
+//For the printResult function here, the current return type is actually VOID, which means the function is not returning anything. 
+//That is because we are only console logging on the inside, we are not returning anything! 
+//In typescript we cannot have the function return undefined, instead we must state it is void. If you state undefined typescript expects you to have a return but with nothing beside it.
+
+function printResult(num: number) {
+    console.log('Result: ' + num);
+}
+
+//This could also be written without type inference where type of return "void" is specifically stated
+
+// function printResult(num: number): void {
+//     console.log('Result: ' + num);
+// }
+
+printResult(add(5, 5));
+
+
---
+++ union-aliases.ts
@@ -0,0 +1,58 @@
+// //Diving into the type of UNIONS
+
+// //Here is our add function from the basics. Let's say we want to be more flexible with what we accept here
+
+// //Let's say we want to adjust the function so it doesn't only add numbers, but it can also concatenate strings as well.
+
+// function combine(input1: number, input2: number) {
+//     //Here we are doing the math caculation before hand. That way we can pass it into the console log, instead of doing n1 + n2 in console log which would coancatenate numbers together due to presence of string in resultphrase
+//     const result = input1 + input2
+//     return result;
+// }
+
+// const combinedAges = combine(30, 26) // This will work and add the numbers
+// console.log(combinedAges)
+
+// //However, what if we want to add a string to concatenate
+
+// const combinedNames = combine('Jordan', 'Nikita') // See error because combine only accepts number, could change function take strings, but then first function call will fail
+
+///////////////////////////////////
+
+//Therefore to deal with these issues we use union types. This allows us to state that the input will take in more than one type
+
+type Combinable = number | string; //This is a type alias, store the union type that the parameter must follow in a type variable, that way can just plug this in and don't need to repeat code. Helps us write code quicker and ensure we don't run into typo issues.
+type ConversionDescriptors = 'as-number' | 'as-text'
+
+function combine(input1: Combinable, input2: Combinable, resultConversion: ConversionDescriptors) { //Resultconversion is example of literal types used in conjunction with union types, they allow for an EXACT value whether string or number to be required as argument for parameter
+    //If we just use union, input1 + input2 will be underlined because typescript doesn't read what is in the union, just assumes it can be anything in which case you couldn't add a boolean and a string or a boolean and a number, so we need to work around
+
+    let result;
+
+    //Here we check if input is all number types, add them
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    } else {
+        //If input is not all number types, can't risk concatinating number and string so make everything a string for consistency and accurate results
+        result = input1.toString() + input2.toString()
+    
+    }
+
+    //Apply a final conversion based on third paramter resultConversion that is passed to function
+    if (resultConversion === 'as-number') {
+        return +result
+    } else {
+        return result.toString()
+    }
+
+    return result;
+}
+
+const combinedAges = combine(30, 26, 'as-number') // This will work and add the numbers
+console.log(combinedAges)
+
+const combinedStringAges = combine('30', '26', 'as-number')
+console.log(combinedStringAges)
+
+const combinedNames = combine('Jordan', 'Nikita', 'as-text')
+console.log(combinedNames)
